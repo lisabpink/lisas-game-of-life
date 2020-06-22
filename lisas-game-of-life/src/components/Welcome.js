245acfa87c9fab9519abad727af6229a0d3d3d94
LISA_COOK <lisabpink@gmail.com>
2020-06-22 17:33:07 -0400

    Welcome page styled

--- lisas-game-of-life/src/components/Welcome.js
+++ lisas-game-of-life/src/components/Welcome.js
@@ -1,22 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Card, CardGroup, Button } from "react-bootstrap";
+import { FaInfoCircle, FaInfinity, FaGamepad, FaHeart } from "react-icons/fa";
+// import { IconName } from "react-icons/bs";
 
 export default function Welcome(props) {
   return (
     <div className="Container">
       <div className="Header">
-        <h1>Conways Game of Life</h1>
+        <h1>
+          I <FaHeart /> Life
+        </h1>
       </div>
       <div className="Welcome">
-        <Link to="/gameoflife">
-          <button>Play!</button>{" "}
-        </Link>
-        <Link to="/rules">
-          <button>Rules</button>{" "}
-        </Link>
-        <Link to="/about">
-          <button>About</button>{" "}
-        </Link>
+        <CardGroup>
+          <Card className="text-center" style={{ margin: "4rem" }}>
+            <Card.Body>
+              <Card.Title>
+                <FaInfoCircle />
+              </Card.Title>
+
+              <Card.Text>
+                Learn all about the history of the really cool super awesome
+                game by reading the about section!
+              </Card.Text>
+              <Card.Link>
+                <Link to="/about">
+                  <Button variant="dark" block>
+                    About
+                  </Button>{" "}
+                </Link>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+
+          <Card className="text-center" style={{ margin: "4rem" }}>
+            <Card.Body>
+              <Card.Title>
+                <FaInfinity />
+              </Card.Title>
+
+              <Card.Text>
+                Learn how to play the really cool super awesome game by reading
+                the rules first!
+              </Card.Text>
+              <Card.Link>
+                <Link to="/rules">
+                  <Button variant="dark" block>
+                    Rules
+                  </Button>{" "}
+                </Link>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+
+          <Card className="text-center" style={{ margin: "4rem" }}>
+            <Card.Body>
+              <Card.Title>
+                <FaGamepad />
+              </Card.Title>
+              <Card.Text>Play this really cool super awesome game!</Card.Text>
+              <Card.Link>
+                <Link to="/gameoflife">
+                  <Button variant="dark" block>
+                    Play!
+                  </Button>{" "}
+                </Link>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </CardGroup>
       </div>
     </div>
   );
