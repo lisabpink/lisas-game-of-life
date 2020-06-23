40e05819786c034fd7c79c88416c9b41b5b1ba10
LISA_COOK <lisabpink@gmail.com>
2020-06-23 14:37:20 -0400

    define cell, nextgen, debug update, add start/stop btns, rendering on screen

--- lisas-game-of-life/src/components/GameOfLife.js
+++ lisas-game-of-life/src/components/GameOfLife.js
@@ -3,6 +3,22 @@ import { Link } from "react-router-dom";
 import { Card, CardGroup, Button } from "react-bootstrap";
 import { FaHome, FaInfoCircle, FaInfinity } from "react-icons/fa";
 
+class Cell {
+  constructor(clickable, living, xcoord, ycoord) {
+    this.clickable = clickable;
+    this.living = living;
+    this.x = xcoord;
+    this.y = ycoord;
+  }
+  toggle = () => {
+    if (this.living === 0) {
+      this.living = 1;
+    } else {
+      this.living = 0;
+    }
+  };
+}
+
 const rows = 50;
 const cols = 50;
 let arr = [];
@@ -11,7 +27,7 @@ for (let i = 0; i < rows; i++) {
 }
 for (let j = 0; j < rows; j++) {
   for (let k = 0; k < cols; k++) {
-    arr[j][k] = 0;
+    arr[j][k] = new Cell(true, 0, 10 * j, 10 * k);
   }
 }
 export default class GameOfLife extends Component {
@@ -26,23 +42,30 @@ export default class GameOfLife extends Component {
       generation: 0
     };
   }
+
+  nextgen = () => {
+    this.setState({ generation: this.state.generation + 1 });
+  };
+
   draw = () => {
     const canv = this.refs.canvas;
     const ctx = canv.getContext("2d");
     for (let x = 0; x < this.width; x += this.cellsize) {
-      ctx.moveto(x, 0);
-      ctx.lineto(x, this.height);
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, this.height);
     }
 
     for (let y = 0; y < this.height; y += this.cellsize) {
-      ctx.moveto(0, y);
-      ctx.lineto(this.width, y);
+      ctx.moveTo(0, y);
+      ctx.lineTo(this.width, y);
     }
+    ctx.strokeStyle = "black";
     ctx.stroke();
 
     for (let j = 0; j < this.height / this.cellsize; j++) {
       for (let k = 0; k < this.width / this.cellsize; k++) {
         if (this.state.cells[j][k] === 1) {
+          ctx.fillStyle = "#FF0000";
           ctx.fillRect(
             j * this.cellsize,
             k * this.cellsize,
@@ -50,6 +73,7 @@ export default class GameOfLife extends Component {
             this.cellsize
           );
         } else {
+          ctx.fillStyle = "black";
           ctx.fillRect(
             j * this.cellsize,
             k * this.cellsize,
@@ -65,15 +89,42 @@ export default class GameOfLife extends Component {
     let mirrorCells = this.state.cells;
     for (let j = 0; j < this.height / this.cellsize; j++) {
       for (let k = 0; k < this.width / this.cellsize; k++) {
+        console.log("Living:", this.state.cells[j][k].living);
+        console.log("J and K:", j, k);
         let liveneighbors = 0;
-        liveneighbors += this.state.cells[j - 1][k - 1].living;
-        liveneighbors += this.state.cells[j - 1][k].living;
-        liveneighbors += this.state.cells[j - 1][k + 1].living;
-        liveneighbors += this.state.cells[j][k - 1].living;
-        liveneighbors += this.state.cells[j][k + 1].living;
-        liveneighbors += this.state.cells[j + 1][k - 1].living;
-        liveneighbors += this.state.cells[j + 1][k].living;
-        liveneighbors += this.state.cells[j + 1][k + 1].living;
+        if (j > 0) {
+          if (k > 0) {
+            console.log("J > 0 K > 0");
+            liveneighbors += this.state.cells[j - 1][k - 1].living;
+          }
+          if (k < this.width / this.cellsize - 1) {
+            console.log("J > 0 K < Width");
+            liveneighbors += this.state.cells[j - 1][k + 1].living;
+          }
+          console.log("J > 0");
+          liveneighbors += this.state.cells[j - 1][k].living;
+        }
+        if (k > 0) {
+          if (j < this.height / this.cellsize - 1) {
+            console.log("K>0 J<height");
+            liveneighbors += this.state.cells[j + 1][k - 1].living;
+          }
+          console.log("K>0");
+          liveneighbors += this.state.cells[j][k - 1].living;
+        }
+        if (j < this.height / this.cellsize - 1) {
+          if (k < this.width / this.cellsize - 1) {
+            console.log("width minus 1", this.width - 1);
+            console.log("J < height K<width");
+            liveneighbors += this.state.cells[j + 1][k + 1].living;
+          }
+          console.log("J < height");
+          liveneighbors += this.state.cells[j + 1][k].living;
+        }
+        if (k < this.width / this.cellsize - 1) {
+          console.log("K<width");
+          liveneighbors += this.state.cells[j][k + 1].living;
+        }
         if (this.state.cells[j][k].living === 0) {
           switch (liveneighbors) {
             case 3:
@@ -111,10 +162,31 @@ export default class GameOfLife extends Component {
   };
 
   tick = () => {
-    this.props.nextgen();
+    if (this.state.continueAnimation) {
+      this.draw();
+      this.update();
+      this.nextgen();
+      requestAnimationFrame(this.tick);
+    }
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    const canv = this.refs.canvas;
+    const lft = canv.offsetLeft;
+    const top = canv.offsetTop;
+    let cells = this.state.cells;
+    canv.addEventListener("click", e => {
+      const x = e.pageX - lft;
+      const y = e.pageY - top;
+      cells.forEach(cell => {
+        if (x > cell.x && x < cell.x + 10 && y > cell.y && y < cell.y + 10) {
+          cell.toggle();
+        }
+      });
+      this.setState({ cells });
+    });
+    requestAnimationFrame(this.tick);
+  }
 
   render() {
     return (
@@ -124,7 +196,11 @@ export default class GameOfLife extends Component {
         </div>
         <p>Pretend theres a game right here- k thx!</p>
         <footer>
-          <canvas ref="canvas" width={this.width} height={this.height} />
+          <div className Game>
+            <Button>Start</Button>
+            <Button>Stop</Button>
+            <canvas ref="canvas" width={this.width} height={this.height} />
+          </div>
           <CardGroup>
             <Card className="text-center" style={{ margin: "4rem" }}>
               <Card.Body>
